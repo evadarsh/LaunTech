d76be650014b2a839e956978c28e872d456d3d89
Adarsh E V <adarshev2002@gmail.com>
2023-11-13 11:55:50 +0530

    Avoided space

--- Project/User/ComplaintBranch.php
+++ Project/User/ComplaintBranch.php
@@ -3,8 +3,6 @@ include("../Assets/Connection/Connection.php");
 session_start();
 ob_start();
 include('Head.php');
-
-
 ?> 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
